ec69b6ace75f74123eae200755e318c9d5186ea2
ChrisWohlert <chriswohlert@hotmail.com>
2020-03-30 12:23:53 +0200

    Indsat VectorIntersection

--- CSharpDiagram/src/PathNavigator.hs
+++ CSharpDiagram/src/PathNavigator.hs
@@ -4,19 +4,18 @@ module PathNavigator where
 
 data Collidable = Collidable { collidePos :: (Double, Double), collideWidth :: Double, collideHeight :: Double }
 
-data StepCollision = StepCollision Collidable
+newtype CollidableInProblemSpace = CollidableInProblemSpace { getCollidable :: Collidable }
 
 type StepPosition = (Double, Double)
+type StepVector = (Double, Double)
+type StepPointVector = (StepPosition, StepVector)
 
 
-data Step = Step { stepPos :: StepPosition
-                 , stepVector :: (Double, Double)
-                 , nextStep :: Step
-                 , stepCollision :: Maybe StepCollision
-                 }
+getPath :: StepPosition -> StepPosition -> [CollidableInProblemSpace] -> [StepPosition]
+getPath = undefined
 
-filterCollidableToProblemSpace :: StepPosition -> StepPosition -> [Collidable] -> [Collidable]
-filterCollidableToProblemSpace (startX, startY) (endX, endY) cs = filter isInside cs
+filterCollidableToProblemSpace :: StepPosition -> StepPosition -> [Collidable] -> [CollidableInProblemSpace]
+filterCollidableToProblemSpace (startX, startY) (endX, endY) = map CollidableInProblemSpace . filter isInside
     where
         isInside (Collidable (x, y) w h) = lowestX < x && highestY > y && highestX > x && lowestY < y
         lowestX = minimum [startX, endX]
@@ -24,4 +23,10 @@ filterCollidableToProblemSpace (startX, startY) (endX, endY) cs = filter isInsid
         highestX = maximum [startX, endX]
         highestY = maximum [startY, endY]
 
-getCollisions :: 
+getVectorIntersection :: StepPointVector -> StepPointVector -> StepPosition 
+getVectorIntersection ((x1, y1), (vx1, vy1)) ((x2, y2), (vx2, vy2)) =
+    let
+        c = vy2 / vx2
+        s = ((y2 - y1) - ((x2 - x1) * c)) / (vy1 - (vx1 * c))
+    in
+        (x1 + vx1 * s, y1 + vy1 * s)
